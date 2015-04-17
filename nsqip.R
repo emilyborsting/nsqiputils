805fc9c0b3ee01f1ec0f958d0b1cd19f55a8a012
emilyborsting <eborsting@miami.edu>
2015-04-17 09:31:37 -0400

    Make filtering by CPT optional.

--- nsqip.R
+++ nsqip.R
@@ -2,20 +2,26 @@
 # install.packages("plyr", repos="http://cran.r-project.org")
 library("plyr")
 
-PrepareNSQIP <- function (cpt,datadir="./data") {
+NSQIPFromDir <- function (cpt,datadir="./data") {
   # Merges all TXT nsqip files in a directory and filters on a given CPT.
   #
   # Args:
-  #   cpt: The CPT code number to filter on.
+  #   cpt: The CPT code number to filter on. Optional, but resulting data is huge.
   #   datadir: Directory containing NSQIP files. Default is `./data`.
   #
   # Returns:
   #   A merged and filtered nsqip datatable by cpt.
   data <- data.frame()
   for(file in list.files(datadir, full.name=TRUE)){
-    data <- join(data,subset(read.delim(file, quote=""), CPT==cpt),
-                 type="full", # Do a full join
-                 match="first")
+    if(missing(cpt)){
+      data <- join(data,read.delim(file, quote=""), type="full", match="first")
+    }else{
+      data <- join(data,subset(read.delim(file, quote=""), CPT==cpt), type="full", match="first")
+    }
   }
   return(data)
 }
+
+# usage
+full.data <- NSQIPFromDir()
+#filtered.by.cpt.data<- NSQIPFromDir(cpt=420029)
