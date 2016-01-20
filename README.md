726981c6c4376226c480315854179595823c62b3
emilyborsting <eborsting@miami.edu>
2016-01-20 09:59:01 -0800

    Support for the new 20124 PUF file.

--- README.md
+++ README.md
@@ -16,25 +16,35 @@ install.packages("RSQLite", repos="http://cran.r-project.org")
 
 ACS NSQIP® provides a set of yearly, self-extracting `TXT`, `SPSS` or `SAS` files. This project uses `TXT`. The upfront effort to use a database is worth the time investment. The following assumes familiarity with unix-like operating systems and standard `gnu-utils`.
 
-### Step 1 : Merge (tab delimited text)
-The extracted `TXT` PUF files are tab delimited and separated by year. To do cross-year analysis we need to merge the dataset. To merge, ensure that each PUF's column headers are of the same case. You can make the headers all uppercase with `vim` by typing `ggvU` then saving `:wq`.
+### Extract 
+
+Put the self-extracting .exe files in a subfolder, `data`. Use the commandline tool unarchiver `unar` to unarchive the executable. 
+    
+    cd ./data
+    find ./ -name \*.exe -exec unar -f {} \;
+
+
+### Merge (tab delimited text)
+The extracted `TXT` PUF files are tab delimited and separated by year. To do cross-year analysis we need to merge the dataset. To merge, ensure that each PUF's column headers are of the same case. You can make the headers all uppercase with `vim` by typing `gUU` then saving `:wq`.
 
 From there, attempt the merge.
+
 ```r
-NSQIPFromDir() # Merge all TXT data, unfiltered
-NSQIPFromDir(cpt=33880) # Merge all TXT data, filtered by CPT code
+data <- NSQIPfromDir() # Merge all TXT data, unfiltered
+write.csv(data, "./data/nsip.csv", row.names=FALSE)
 ```
+
 Merging all datasets can take over an hour. Merging by filtered CPT code takes closer to 10 minutes. Fortunately, there are faster ways to query and filter data.
 
-### Step 2 : Clean (csv)
+### Clean (csv)
 Save the previously merged data as a CSV file. Remove `"NULL"` and `-99` as NULL values in CSV are simply represented by an empty field.
-```sh
-# replace "NULL" and -99; save the new file as nsqip.nulls.99.csv
-sed -e 's/\"NULL\"//g' -e 's/-99//g' nsqip.csv > nsqip.nulls.99.csv
-# compress the new CSV file with gzip
-gzip nsqip.nulls.99.csv
-```
-The merged, cleaned, and compressed `nsqip.nulls.99.csv.gz` is ~500MB. The combined CSV is still consumable by `R` but filtering is still slow.
+
+    # replace "NULL" and -99; save the new file as nsqip-cleaned.csv
+    sed -e 's/\"NULL\"//g' -e 's/-99//g' nsqip.csv > nsqip-cleaned.csv
+    # (optionally) compress the new CSV file with gzip
+    gzip nsqip-cleaned.csv
+
+The merged, cleaned, and compressed `nsqip-cleaned.csv` is ~500MB. The combined CSV is still consumable by `R` but filtering is still slow.
 
 ### Step 3 : Query (sql)
 For faster querying, use a database.
@@ -47,7 +57,7 @@ Open the database, then import the cleaned and merged CSV file.
 ```sh
 sqlite3 data/nsqip.db 
 sqlite> .mode csv NSQIP
-sqlite> .import data/nsqip.nulls.99.csv NSQIP
+sqlite> .import data/nsqip-cleaned.csv NSQIP
 ```
 The database file is ~3.8 GB. Querying is extremely fast; filtering by CPT code has gone from 10 minutes to less than a tenth of a second.
 
--- nsqip.R
+++ nsqip.R
@@ -6,91 +6,137 @@ library("plyr")
 library("RSQLite")
 
 
-NSQIPFromDir <- function (cpt,datadir="./data") {
-  # Args:
-  #   cpt: The CPT code number to filter on. Optional, but resulting data is huge.
-  #   datadir: Directory containing NSQIP files. Default is `./data`.
-  #
-  # Returns:
-  #   A merged and filtered nsqip datatable by cpt.
-  data <- data.frame()
-  for(file in list.files(datadir, full.name=TRUE)){
-    if(missing(cpt)){
-      data <- join(data,read.delim(file, quote=""), type="full", match="first")
-    }else{
-      data <- join(data,subset(read.delim(file, quote=""), CPT==cpt), type="full", match="first")
-    }
-  }
-  return(data)
-}
-
-NSQIPFromCSV <- function (cpt,file="./data/nsqip.csv.gz") {
-  # Args:
-  #   cpt: The CPT code number to filter on. Optional, but resulting data is huge.
-  #   file: File containing NSQIP data. Default is `./data/nsqip.csv.gz`.
-  #
-  # Returns:
-  #   A filtered nsqip data frame by cpt.
-  data <- data.frame()
+NSQIPfromDir <- function (cpt,datadir="./data") {
+ # Args:
+ #   cpt: The CPT code number to filter on. Optional, but resulting data is huge.
+ #   datadir: Directory containing NSQIP files. Default is `./data`.
+ #
+ # Returns:
+ #   A merged and filtered nsqip datatable by cpt.
+ data <- data.frame()
+ for(file in list.files(datadir, full.name=TRUE)){
   if(missing(cpt)){
-    data <- join(data,read.delim(file, quote=""), type="full", match="first")
+   data <- join(data,read.delim(file, quote=""), type="full", match="first")
   }else{
-    data <- join(data,subset(read.delim(file, quote=""), CPT==cpt), type="full", match="first")
+   data <- join(data,subset(read.delim(file, quote=""), CPT==cpt), type="full", match="first")
   }
-  return(data)
+ }
+ return(data)
 }
 
-ByCPT <- function (cpt,con) {
-  # Args:
-  #   cpt: The CPT code number to filter on.
-  #   con: Database connection
-  #
-  # Returns:
-  #   A filtered nsqip data frame by cpt.
-  
-  res <- dbSendQuery(con, sprintf("SELECT * FROM NSQIP WHERE CPT = %s",cpt))
-  data <- dbFetch(res,n=-1)
-  dbClearResult(res)
-  return(data)
+NSQIPfromCSV <- function (cpt,file="./data/nsqip.csv.gz") {
+ # Args:
+ #   cpt: The CPT code number to filter on. Optional, but resulting data is huge.
+ #   file: File containing NSQIP data. Default is `./data/nsqip.csv.gz`.
+ #
+ # Returns:
+ #   A filtered nsqip data frame by cpt.
+ data <- data.frame()
+ if(missing(cpt)){
+  data <- join(data,read.delim(file, quote=""), type="full", match="first")
+ }else{
+  data <- join(data,subset(read.delim(file, quote=""), CPT==cpt), type="full", match="first")
+ }
+ return(data)
 }
 
-ByMultipleCPT <- function (cpts,con) {
-  # Args:
-  #   cpts: A collection of CPT codes to filter on.
-  #   con: Database connection
-  #
-  # Returns:
-  #   A filtered nsqip data frame by a collection of cpt codes.
-  qry <- sprintf("SELECT * FROM NSQIP WHERE ")
-  i<-0
-  for (cpt in cpts){
-    if(i==0){
-      qry <- sprintf("%s CPT = %s",qry, cpt)
-    }else{
-      qry <- sprintf("%s OR CPT = %s",qry, cpt)
-    }
-    i<-i+1
+ByCPTCode <- function (cpt,con) {
+ # Args:
+ #   cpt: The CPT code number to filter on.
+ #   con: Database connection
+ #
+ # Returns:
+ #   A filtered nsqip data frame by cpt.
+ 
+ res <- dbSendQuery(con, sprintf("select * from NSQIP where CPT = %s",cpt))
+ data <- dbFetch(res,n=-1)
+ dbClearResult(res)
+ return(data)
+}
+
+ByCPTCodes <- function (cpts,con) {
+ # Args:
+ #   cpts: A collection of CPT codes to filter on.
+ #   con: Database connection
+ #
+ # Returns:
+ #   A filtered nsqip data frame by a collection of cpt codes.
+ #   Null gender, non-plastic surgery specialities are excluded. 
+ qry <- sprintf("select * 
+                from NSQIP 
+                where SURGSPEC = 'Plastics'
+                and SEX is not null
+                and ( ")
+ i<-0
+ for (cpt in cpts){
+  if(i==0){
+   qry <- sprintf("%s CPT = %s",qry, cpt)
+  }else{
+   qry <- sprintf("%s or CPT = %s",qry, cpt)
   }
-  res <- dbSendQuery(con, qry)
-  data <- dbFetch(res,n=-1)
-  dbClearResult(res)
-  return(data)
+  i<-i+1
+ }
+ qry <- sprintf("%s )",qry, cpt)
+ res <- dbSendQuery(con, qry)
+ data <- dbFetch(res,n=-1)
+ dbClearResult(res)
+ return(data)
+}
+
+TotalPatients <- function (cont) {
+ res <- dbSendQuery(con, sprintf("select count(*) from NSQIP"))
+ data <- dbFetch(res,n=-1)
+ dbClearResult(res)
+ return(as.numeric(data))
+}
+
+TotalPatientsMale <- function (cont) {
+ res <- dbSendQuery(con, sprintf("select count(*) from NSQIP where SEX = 'male'"))
+ data <- dbFetch(res,n=1)
+ dbClearResult(res)
+ return(as.numeric(data))
+}
+
+TotalPatientsFemale <- function (cont) {
+ res <- dbSendQuery(con, sprintf("select count(*) from NSQIP where SEX = 'female'"))
+ data <- dbFetch(res,n=1)
+ dbClearResult(res)
+ return(as.numeric(data))
+}
+
+SumPRSbyCPT <- function (cont) {
+ res <- dbSendQuery(con, sprintf("select CPT, PRNCPTX, count(*) as num
+                                 from NSQIP
+                                 where SURGSPEC = 'Plastics'
+                                 group by CPT
+                                 order by num"))
+ data <- dbFetch(res,n=-1)
+ dbClearResult(res)
+ return(data)
 }
 # Usage
+setwd("~/Dropbox/Research/nsqip-utils")
 
 # TXT -------------------------------------
-#full.data.txt <- NSQIPFromDir()
-#cpt.data.txt <- NSQIPFromDir(cpt=33880)
+full.data.txt <- NSQIPfromDir()
+#cpt.data.txt <- NSQIPfromDir(cpt=33880)
 
 # CSV -------------------------------------
-#full.data.csv <- NSQIPFromCSV()
-#cpt.data.csv <- NSQIPFromCSV(cpt=33880)
+#full.data.csv <- NSQIPfromCSV()
+#cpt.data.csv <- NSQIPfromCSV(cpt=33880)
 
 # SQL -------------------------------------
-con <- dbConnect(RSQLite::SQLite(),"./data/nsqip.db")
+#con <- dbConnect(RSQLite::SQLite(),"./data/nsqip.db")
 #cpt.data.sql <- ByCPT(cpt=15610,con)
-cpt.codes <- c(15756,15757,15738)
-cpt.data.sql <- ByMultipleCPT(cpts=cpt.codes,con)
-# Excel exporting
-csv.file.name <- paste(cpt.codes, collapse='-' )
-write.csv(cpt.data.sql, file = sprintf("nsqip.codes.%s.csv",csv.file.name),row.names=FALSE, na="")
+#cpt.codes <- c(15756,15757,15738)
+#cpt.data.sql <- ByCPTCodes(cpts=cpt.codes,con)
+#nsqip.total.patients <-TotalPatients(con)
+#aggregate.cpts.by.prs<-SumPRSbyCPT(con)
+#nsqip.total.patients.male <-TotalPatientsMale(con)
+# nsqip.total.patients.female <-TotalPatientsFemale(con)
+
+# Excel  -------------------------------------
+
+
+#csv.file.name <- paste(aggregate.cpts.by.prs, collapse='-' )
+#write.csv(aggregate.cpts.by.prs, file = sprintf("nsqip.count.by.procedure.csv",csv.file.name),row.names=FALSE, na="")
